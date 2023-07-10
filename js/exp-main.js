bfd43342047cd7c9dbd3b461bef0aab563f3e92f
prakriti5dhang <prakritidhang@gmail.com>
2023-07-10 17:51:19 +0530

    SE-exp1

---
+++ js/exp-main.js
@@ -0,0 +1,13 @@
+/*
+Lab: 
+Exp:
+File Name: main.js
+Author: Prakriti Dhang*/
+
+$(document).ready(function() {
+  $('.navbar-nav .nav-link').click(function() {
+    $('.navbar-nav .nav-link').removeClass('active'); // Remove active class from all links
+    $(this).addClass('active'); // Add active class to clicked link
+  });
+});
+
